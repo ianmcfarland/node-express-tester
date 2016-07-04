cf577ba38fdda7607e2196b11d361477544cbeae
imcfarland <imcfarland@telnetww.com>
2016-07-04 11:58:54 -0400

    Rename app.js to server.js.

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 
-var app = express();
+var server = express();
 var port = process.env.PORT || 5000;
 
 // "Middleware"
-app.use(express.static('public'));
-app.set('views', 'src/views');
-app.set('view engine', 'ejs');
+server.use(express.static('public'));
+server.set('views', 'src/views');
+server.set('view engine', 'ejs');
 
 // Set up routes
 var router = require('./src/routes');
-app.use('/', router);
+server.use('/', router);
 
-app.listen(port, function (err) {
+server.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
