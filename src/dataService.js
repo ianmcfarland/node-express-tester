1b984da259c4bca788d5c138fc36a330eb89d9a1
imcfarland <imcfarland@telnetww.com>
2016-07-06 08:57:04 -0400

    Added DB config file.

--- src/dataService.js
+++ src/dataService.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
-var dbUrl = 'mongodb://localhost:27017/test';
+var dbUrl = require('../config/database').url;
 var db;
 
 MongoClient.connect(dbUrl, function(err, database) {
@@ -9,6 +9,7 @@ MongoClient.connect(dbUrl, function(err, database) {
         console.log('Error connecting to database: ' + err);
         throw err;
     } else {
+        console.log('Connected to URL: ' + dbUrl);
         db = database;
     }
 });
